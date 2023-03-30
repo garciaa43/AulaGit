a52bae8e2bbd5d4005f6fce105fe101e6cec3519
gabriel.cordeiro1 <gabriel.cordeiro1@FLE207D10.alunos.sc.senac.br>
2023-03-30 10:59:04 -0300

    Feito classe BO

--- src/main/java/model/bo/ClienteBO.java
+++ src/main/java/model/bo/ClienteBO.java
@@ -2,18 +2,19 @@ package model.bo;
 
 import model.dao.telefonia.ClienteDAO;
 import model.exception.CpfJaUtilizadoException;
+import model.exception.EnderecoInvalidoException;
 import model.vo.telefonia.Cliente;
 
 public class ClienteBO {
 
 	private ClienteDAO dao = new ClienteDAO();
 	
-	public Cliente inserir(Cliente novoCliente) throws CpfJaUtilizadoException{
+	public Cliente inserir(Cliente novoCliente) throws CpfJaUtilizadoException, EnderecoInvalidoException{
 		if(dao.cpfJaUtilizado(novoCliente.getCpf())) {
 			throw new CpfJaUtilizadoException("CPF informado ja fui utilizado");
 		} 
 	
-		if(novoCliente.getEndereco() == null) {
+		if(novoCliente.getEndereco() == null || novoCliente.getEndereco().getId() == null) {
 			throw new EnderecoInvalidoException("Endereço não informado")
 		}
 		
